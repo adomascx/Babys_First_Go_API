09cbc6b9d981d99be47e74606fcffd4f93409cf9
adomascx <adlukosev2@gmail.com>
2026-06-05 21:45:02 +0300

    Fix old handler implementation, separate handlers into own package, update Go module files

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,65 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
-	"sync"
+
+	"github.com/adomascx/Skelbiu_API/internal/handler"
 )
-
-var (
-	users []User
-	mutex sync.Mutex
-)
-
-// Return Types
-type User struct {
-	ID      int    `json:"id"`
-	Name    string `json:"name"`
-	Balance int    `json:"balance"`
-}
-
-// Handlers
-func GetHealth(w http.ResponseWriter, r *http.Request) {
-	log.Println("health request received")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
-}
-
-func GetUser(w http.ResponseWriter, r *http.Request) {
-	log.Println("GET user request received")
-	w.WriteHeader(http.StatusOK) // maybe a different status should be used?
-	w.Write([]byte{})            // temporarily
-}
-
-func CreateUser(w http.ResponseWriter, r *http.Request) {
-	log.Println("POST user request received")
-
-	var user User
-
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	mutex.Lock()
-	user.ID = len(users) + 1
-	users = append(users, user)
-	mutex.Unlock()
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(user)
-}
 
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/health", GetHealth)
-
-	mux.HandleFunc("GET /user", GetUser)
-	mux.HandleFunc("POST /user", CreateUser)
+	mux.HandleFunc("/health", handler.GetHealth)
+	mux.HandleFunc("GET /listings", handler.GetListings)
 
 	// HTTP server
 	port := os.Getenv("PORT")
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,30 @@
 package handler
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/adomascx/Skelbiu_API/internal/model"
+)
+
+var (
+	listings []model.Listing
+	mutex    sync.Mutex
+)
+
+// Handlers
+func GetHealth(w http.ResponseWriter, r *http.Request) {
+	log.Println("health request received")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func GetListings(w http.ResponseWriter, r *http.Request) {
+	log.Println("GET listing request received")
+
+	// scraper.ScrapeListings()
+
+	w.WriteHeader(http.StatusNotImplemented)
+	w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
+}
