a623c47fab3e095f02e9c0476094014ab91e631b
adomascx <adlukosev2@gmail.com>
2026-06-08 18:14:23 +0300

    Implement Listings filter method and corresponding tests

--- internal/model/listing.go
+++ internal/model/listing.go
@@ -7,3 +7,21 @@ type Listing struct {
 	Date        string  `json:"date"`        // date since last update to listing, in plaintext
 	Price       float64 `json:"price"`       // price in €
 }
+
+type Listings []Listing
+
+func (listings *Listings) Filter() {
+	len := len(*listings)
+	filtered := make([]Listing, 0, len)
+	seen := make(map[Listing]bool, len)
+
+	// remove duplicates
+	for _, listing := range *listings {
+		if !seen[listing] {
+			seen[listing] = true
+			filtered = append(filtered, listing)
+		}
+	}
+
+	*listings = filtered
+}
--- internal/model/model_test.go
+++ internal/model/model_test.go
@@ -1,10 +1,13 @@
 package model_test
 
 import (
+	"reflect"
 	"testing"
 
 	. "github.com/adomascx/Skelbiu_API/internal/model"
 )
+
+// Query
 
 func TestQueryString(t *testing.T) {
 	want := "category_id=80&cities=465%2C43&condition=2&cost_max=420&cost_min=69&keywords=test&type=1&user_type=1"
@@ -48,5 +51,80 @@ func BenchmarkQueryStringEmpty(b *testing.B) {
 
 	for b.Loop() {
 		_ = query.String()
+	}
+}
+
+// Listings
+
+var testListings = Listings{
+	Listing{Link: "aaa", Price: 45.6, Title: "aaaTitle"},
+	Listing{Link: "bbb", Price: 12.3, Title: "bbbTitle"},
+	Listing{Link: "ccc", Price: 45.6, Title: "cccTitle"},
+	Listing{Link: "ddd", Price: 12.3, Title: "dddTitle"},
+	Listing{Link: "aaa", Price: 45.6, Title: "aaaTitle"},
+	Listing{Link: "eee", Price: 12.3, Title: "eeeTitle"},
+	Listing{Link: "fff", Price: 45.6, Title: "fffTitle"},
+	Listing{Link: "bbb", Price: 12.3, Title: "bbbTitle"},
+}
+
+func TestFilter(t *testing.T) {
+	want := Listings{
+		Listing{Link: "aaa", Price: 45.6, Title: "aaaTitle"},
+		Listing{Link: "bbb", Price: 12.3, Title: "bbbTitle"},
+		Listing{Link: "ccc", Price: 45.6, Title: "cccTitle"},
+		Listing{Link: "ddd", Price: 12.3, Title: "dddTitle"},
+		Listing{Link: "eee", Price: 12.3, Title: "eeeTitle"},
+		Listing{Link: "fff", Price: 45.6, Title: "fffTitle"},
+	}
+
+	have := Listings{
+		Listing{Link: "aaa", Price: 45.6, Title: "aaaTitle"},
+		Listing{Link: "bbb", Price: 12.3, Title: "bbbTitle"},
+		Listing{Link: "ccc", Price: 45.6, Title: "cccTitle"},
+		Listing{Link: "ddd", Price: 12.3, Title: "dddTitle"},
+		Listing{Link: "aaa", Price: 45.6, Title: "aaaTitle"},
+		Listing{Link: "eee", Price: 12.3, Title: "eeeTitle"},
+		Listing{Link: "fff", Price: 45.6, Title: "fffTitle"},
+		Listing{Link: "bbb", Price: 12.3, Title: "bbbTitle"},
+	}
+
+	have.Filter()
+
+	if !reflect.DeepEqual(have, want) {
+		t.Errorf("Filter() - did not filter resuts as expected:\nHave = %v\nWant =  %v", have, want)
+	}
+}
+
+func BenchmarkStructMapDedupe(b *testing.B) {
+
+	for b.Loop() {
+
+		len := len(testListings)
+		filtered := make([]Listing, len)
+		seen := make(map[Listing]bool, len)
+
+		for _, listing := range testListings {
+			if !seen[listing] {
+				seen[listing] = true
+				filtered = append(filtered, listing)
+			}
+		}
+	}
+}
+
+func BenchmarkLinkMapDedupe(b *testing.B) {
+
+	for b.Loop() {
+
+		len := len(testListings)
+		filtered := make([]Listing, len)
+		seen := make(map[string]bool, len)
+
+		for _, listing := range testListings {
+			if !seen[listing.Link] {
+				seen[listing.Link] = true
+				filtered = append(filtered, listing)
+			}
+		}
 	}
 }
