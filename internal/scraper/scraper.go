aa9e3742035fbf09c6e93e3460172849d67f4f4d
adomascx <adlukosev2@gmail.com>
2026-06-08 20:50:15 +0300

    Fix and finish implementing scraper function w/ uTLS. Implement tests for helper function and improve main scraping function testing

--- internal/scraper/scraper.go
+++ internal/scraper/scraper.go
@@ -1,7 +1,13 @@
 package scraper
 
 import (
+	"context"
+	"crypto/tls"
 	"errors"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
@@ -9,63 +15,170 @@ import (
 	"time"
 
 	"github.com/adomascx/Skelbiu_API/internal/model"
-	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/v2"
+	utls "github.com/refraction-networking/utls"
+	"golang.org/x/net/http2"
 )
 
-const SKELBIU_URL = "https://www.skelbiu.lt/skelbimai/"
+const (
+	skelbiuURL      = "https://www.skelbiu.lt/skelbimai/"
+	skelbiuHost     = "www.skelbiu.lt"
+	chromeUA        = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+	listingsPerPage = 24
+	defaultPages    = 5
+)
 
 // If pages is set to 0, defaults to 5
-func ScrapeListings(query model.QueryParams, pages int) ([]model.Listing, error) {
+func ScrapeListings(query model.QueryParams, pages int) (model.Listings, error) {
 	if query == (model.QueryParams{}) {
-		return nil, errors.New("No query given")
+		return nil, errors.New("no query given")
 	}
 	if pages < 0 {
-		return nil, errors.New("Pages must be >= 0")
+		return nil, errors.New("pages must be >= 0")
 	}
 
 	// Default pages value and error handling
 	if pages == 0 {
-		pages = 5
+		pages = defaultPages
 	}
 
 	// Listings init
-	listings := make([]model.Listing, 24*pages)
+	listings := make([]model.Listing, 0, listingsPerPage*pages)
 
 	// Set concurrency based on env file. May result in blocked requests.
 	// Defaults to 'false'
 	useConcurrency := strings.EqualFold(os.Getenv("USE_CONCURRENCY"), "true")
-	collector := colly.NewCollector(colly.Async(useConcurrency))
 
-	collector.Limit(&colly.LimitRule{
+	// Set up colly collector
+	collector := colly.NewCollector(
+		colly.Async(useConcurrency),
+		colly.UserAgent(chromeUA),
+		colly.AllowedDomains(skelbiuHost),
+	)
+
+	err := collector.Limit(&colly.LimitRule{
+		DomainGlob:  skelbiuHost,
 		Parallelism: 2,
 		Delay:       1 * time.Second,
+		RandomDelay: 500 * time.Millisecond,
 	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Create dialer raw TCP connection
+	dialer := &net.Dialer{
+		Timeout:   15 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+
+	// Connect and wrap with uTLS
+	utlsTransport := &http2.Transport{
+		DialTLSContext: func(ctx context.Context, network, addr string, cfg *tls.Config) (net.Conn, error) {
+			// Open TCP connection
+			rawConn, err := dialer.DialContext(ctx, network, addr)
+			if err != nil {
+				rawConn.Close()
+				return nil, fmt.Errorf("could not do dialer.DialContext(): %w\n", err)
+			}
+
+			// Configure uTLS
+			utlsConfig := &utls.Config{
+				ServerName: skelbiuHost,
+				NextProtos: []string{"h2", "http/1.1"},
+				MinVersion: utls.VersionTLS12,
+			}
+
+			// Create uTLS connection
+			uconn := utls.UClient(rawConn, utlsConfig, utls.HelloChrome_Auto)
+
+			// Perform TLS handshake
+			err = uconn.HandshakeContext(ctx)
+			if err != nil {
+				return nil, fmt.Errorf("could not do uconn.HandshakeContext(%v): %w\n", ctx, err)
+			}
+
+			return uconn, nil
+		},
+	}
+
+	// Attach uTLS connection to Colly
+	utlsClient := &http.Client{
+		Timeout:   30 * time.Second,
+		Transport: utlsTransport,
+	}
+
+	collector.SetClient(utlsClient)
 
 	var mutex sync.Mutex
 
+	collector.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+		r.Headers.Set("Accept-Language", "lt-LT,lt;q=0.9,en-US;q=0.8,en;q=0.7")
+
+		log.Println("Currently scraping:", r.URL.Path)
+	})
+
+	collector.OnHTML(".standard-list-item", func(h *colly.HTMLElement) {
+		// filter sold items
+		if h.DOM.Find(".item.sold").Length() > 0 {
+			return
+		}
+
+		listing := model.Listing{
+			Link:        h.Request.AbsoluteURL(h.Attr("href")),
+			Title:       h.ChildText(".title"),
+			Description: h.ChildText(".first-dataline"),
+			Date:        h.ChildText(".second-dataline"),
+		}
+
+		price, _ := parsePrice(h.ChildText(".price"))
+		listing.Price = price
+
+		mutex.Lock()
+		listings = append(listings, listing)
+		mutex.Unlock()
+	})
+
+	collector.OnError(func(r *colly.Response, err error) {
+		log.Printf("colly encountered a problem when scraping: %v\n%+v", err, r)
+	})
+
 	for page := 1; page <= pages; page++ {
 
-		collector.OnHTML(".standard-list-item", func(h *colly.HTMLElement) {
-			listing := model.Listing{
-				Link:        h.Attr("href"),
-				Title:       h.ChildText(".title"),
-				Description: h.ChildText(".first-dataline"),
-				Date:        h.ChildText(".second-dataline"),
-			}
-
-			Price, _ := strconv.ParseFloat(h.ChildText(".price"), 64)
-			listing.Price = Price
-
-			mutex.Lock()
-			listings = append(listings, listing)
-			mutex.Unlock()
-		})
-
-		url := SKELBIU_URL + strconv.Itoa(page) + "?" + query.String()
-		collector.Visit(url)
-
-		collector.Wait()
+		url := skelbiuURL + strconv.Itoa(page) + "?" + query.String()
+		err := collector.Visit(url)
+		if err != nil {
+			log.Printf("could not do Visit(%v): %v\n", url, err)
+			continue
+		}
 	}
 
+	collector.Wait()
+
 	return listings, nil
+}
+
+func parsePrice(priceStr string) (float64, error) {
+	var cleanedStr strings.Builder
+
+	for _, rune := range priceStr {
+		// change decimal point to Go float standard
+		if rune == ',' {
+			rune = '.'
+		} else if rune < '0' || rune > '9' { // only accept digits 0-9
+			continue
+		}
+
+		cleanedStr.WriteRune(rune)
+	}
+
+	// if price not provided, return default value of 0
+	if cleanedStr.String() == "" {
+		return 0, nil
+	}
+
+	output, err := strconv.ParseFloat(cleanedStr.String(), 64)
+
+	return output, err
 }
--- internal/scraper/scraper_test.go
+++ internal/scraper/scraper_test.go
@@ -1,18 +1,65 @@
 package scraper
 
 import (
-	"fmt"
+	"os"
 	"testing"
 
 	"github.com/adomascx/Skelbiu_API/internal/model"
+	"github.com/joho/godotenv"
 )
 
-// Temp non-functional test
-func TestGetSkelbiuHTML(t *testing.T) {
-	result, err := ScrapeListings(model.QueryParams{}, 1)
+func TestMain(m *testing.M) {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		panic(err)
+	}
+
+	os.Exit(m.Run())
+}
+
+func TestEnvLoad(t *testing.T) {
+	env := os.Getenv("USE_CONCURRENCY")
+	if env != "true" && env != "false" {
+		t.Errorf("env loading - variable not present or empty:\nHave = %v\nWant =  %v", os.Getenv("USE_CONCURRENCY"), "true or false")
+	}
+}
+
+// Non-standard test, only checks for error
+// The only logical way to implement actual tests would be to mimic Skelbiu.lt's HTTP endpoint, which is out of scope for this project
+func TestScrapeListings(t *testing.T) {
+	_, err := ScrapeListings(model.QueryParams{CategoryID: 80}, 1)
 	if err != nil {
 		t.Errorf("could not do ScrapeListings(): %v\n", err)
 	}
+}
 
-	fmt.Println(result)
+func TestParsePrice(t *testing.T) {
+	testCases := []struct {
+		want  float64
+		input string
+	}{
+		{
+			want:  1234.5,
+			input: "1 234,5€",
+		},
+		{
+			want:  123456789.0,
+			input: "kaina =  1 2 3 4 5 6 7 8 9 , 0 eurai/€",
+		},
+		{
+			want:  0,
+			input: "kaina sutartine",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.input, func(t *testing.T) {
+			have, err := parsePrice(tC.input)
+			if err != nil {
+				t.Errorf("parsePrice() - returned error:\nHave = %v\nWant =  %v\nError =  %v", have, tC.want, err)
+			}
+			if tC.want != have {
+				t.Errorf("parsePrice() - did not parse price as expected:\nHave = %v\nWant =  %v", have, tC.want)
+			}
+		})
+	}
 }
